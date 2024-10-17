a47f1f88717583f52d839903e73e5250b8abdda9
Josh Daniel <jd8287dan@proton.me>
2024-10-17 14:15:43 -0400

    path and mason config

--- .config/nvim/lua/config/mason.lua
+++ .config/nvim/lua/config/mason.lua
@@ -7,3 +7,13 @@ require("mason").setup({
         }
     }
 })
+require('mason-lspconfig').setup({
+  -- Replace the language servers listed here
+  -- with the ones you want to install
+  ensure_installed = {'rust_analyzer'},
+  handlers = {
+    function(server_name)
+      require('lspconfig')[server_name].setup({})
+    end,
+  }
+})
